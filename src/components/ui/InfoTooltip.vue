320df646c5e6f163fa4a3b29b639b8ce202a91dd
migliorellI <miglenti@gmail.com>
2025-01-28 01:24:17 -0300

    rewrite info tooltip position logic

--- src/components/ui/InfoTooltip.vue
+++ src/components/ui/InfoTooltip.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { Info } from "lucide-vue-next";
-import { ref, useTemplateRef, watch } from "vue";
-import useDimensions from "../../composables/useDimensions";
+import { onMounted, onUnmounted, ref, useTemplateRef } from "vue";
 import Button from "../ui/Button.vue";
 
 interface Props {
@@ -12,7 +11,6 @@ const props = defineProps<Props>();
 const open = ref(false);
 const coords = ref<[number, number]>([0, 0]);
 const button = useTemplateRef("infoButton");
-const dimensions = useDimensions();
 
 const toggleInfo = () => {
   if (!props.disabled) {
@@ -30,12 +28,16 @@ const closeInfo = (event: MouseEvent) => {
   }
 };
 
-watch([button, dimensions], () => {
-  if (button.value) {
-    const rect = button.value.$el.getBoundingClientRect();
-    coords.value = [rect.top + rect.height + 12, rect.left + rect.width / 2];
-  }
-});
+const updateTooltipCoords = () => {
+  if (!button.value) return;
+
+  const rect = button.value.$el.getBoundingClientRect();
+  coords.value = [rect.top + rect.height + 12, rect.left + rect.width / 2];
+};
+
+const resizeObserver = new ResizeObserver(updateTooltipCoords);
+onMounted(() => resizeObserver.observe(document.body));
+onUnmounted(() => resizeObserver.disconnect());
 </script>
 
 <template>
@@ -50,19 +52,17 @@ watch([button, dimensions], () => {
   </Button>
 
   <Teleport to="body">
-    <Transition>
-      <div
-        v-click-outside="closeInfo"
-        v-if="open"
-        class="info-balloon"
-        :style="{
-          top: `${coords[0]}px`,
-          left: `${coords[1]}px`,
-        }"
-      >
-        Click on any skin to remove it from the draw
-      </div>
-    </Transition>
+    <div
+      v-click-outside="closeInfo"
+      :class="['info-balloon', { open }]"
+      :aria-hidden="open"
+      :style="{
+        top: `${coords[0]}px`,
+        left: `${coords[1]}px`,
+      }"
+    >
+      Click on any skin to remove it from the draw
+    </div>
   </Teleport>
 </template>
 
@@ -72,13 +72,16 @@ watch([button, dimensions], () => {
   z-index: 50;
   text-align: justify;
   font-size: small;
-  max-width: 200px;
+  width: 200px;
   background-color: white;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.3);
   color: black;
   padding: 10px;
   border-radius: 15px;
+  transition: all 200ms;
   border-top-right-radius: 0px;
+  opacity: 0;
+  visibility: hidden;
   transform: translateX(-100%);
 }
 
@@ -93,19 +96,8 @@ watch([button, dimensions], () => {
   border-bottom: 10px solid white;
 }
 
-.v-enter-active,
-.v-leave-active {
-  transition: opacity 300ms ease;
-}
-
-.v-enter-from,
-.v-leave-to {
-  opacity: 0;
-}
-
-@media (max-width: 1024px) {
-  .info-balloon {
-    max-width: 100dvw;
-  }
+.info-balloon.open {
+  opacity: 1;
+  visibility: visible;
 }
 </style>
