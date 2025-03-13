a8ae7165a2126e1f9b801fb16447093451467e4f
Miguel Migliorelli Bringhenti <miglenti@gmail.com>
2025-03-12 22:12:15 -0300

    configured vite.config.ts for deploying

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
+import sitemap from "vite-plugin-sitemap";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    sitemap({
+      hostname: "https://lolrandom.migliorelli.dev",
+      outDir: "dist",
+    }),
+  ],
+  server: {
+    port: 3000,
+    strictPort: true,
+    host: true,
+    watch: {
+      usePolling: true,
+    },
+  },
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    emptyOutDir: true,
+    sourcemap: false,
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+  },
 });
